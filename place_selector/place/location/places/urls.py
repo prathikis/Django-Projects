bbfa36c2a2dccb2f395fb4abb7157ee61d8b1763
prathik i s <76772803+prathikis@users.noreply.github.com>
2024-10-04 22:56:16 +0530

    Delete place_selector/place/location directory

--- place_selector/place/location/places/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-app_name = 'places'
-
-urlpatterns = [
-    path('', views.place_view, name='place_view'),
-    path('get_place_hierarchy/', views.get_place_hierarchy, name='get_place_hierarchy'),
-]
